1a5123149b240a9efd3f110d3e1910f0e252755f
Tomasi - Developing <info@tomasi-developing.ch>
2022-11-02 19:58:39 +0100

    base component for contact, services and login

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./home/home.component";
+import {ServicesComponent} from "./services/services.component";
+import {ContactComponent} from "./contact/contact.component";
+import {LoginComponent} from "./login/login.component";
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent},
+  { path: 'services', component: ServicesComponent},
+  { path: 'contact', component: ContactComponent},
+  { path: 'login', component: LoginComponent},
   { path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
 
--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -6,12 +6,18 @@ import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 import {AppRoutingModule} from "./app-routing.module";
 import { NavigationComponent } from './navigation/navigation.component';
 import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
-    HomeComponent
+    HomeComponent,
+    ServicesComponent,
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
